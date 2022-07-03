a0ad085eb1de46379fef9d04201d32a55952adfe
ClintTheEngineer <reachme@clintalexander.dev>
2022-07-02 20:18:40 -0400

    parser added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 app.use(express.json());
 //app.use(cors());
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
